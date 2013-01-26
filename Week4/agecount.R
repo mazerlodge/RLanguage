7857d77ad60d4a71d31993a83da57d8847cf3ebd
mazerlodge <mazerlodge@hotmail.com>
2013-01-26 16:34:11 -0600

    With Week 4 Part 2 working.

--- Week4/agecount.R
+++ Week4/agecount.R
@@ -1,11 +1,29 @@
 agecount <- function(age = NULL) {
 	## Check that "age" is non-NULL; else throw error
+	if (is.null(age)) {
+		stop("No age provided.")
+	}
 
 	## Read "homicides.txt" data file
+	f <- "homicides.txt"
+	ds <- readLines(f)
 
 	## Extract ages of victims; ignore records where no age is
 	## given
+	pattern <- "(..) years old"
+	posIdx <- regexec(pattern,ds)
+	m <- regmatches(ds,posIdx)
+	
+	matchCount <- 0
+	for (x in 1:length(m)) {
+		if (is.na(m[[x]][1])) next
+		sl <- m[[x]]
+		if (sl[2] == age) {
+			matchCount <- matchCount + 1
+		}
+	}
 
 	## Return integer containing count of homicides for that age
-
+	return(matchCount)
+	
 }
